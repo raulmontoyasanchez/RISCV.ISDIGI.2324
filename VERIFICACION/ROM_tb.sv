5c03d6cc0aae84f1ea65bc64ae68b194e72dba1b
dgargon2 <dgarglez03@gmail.com>
2023-12-07 20:30:46 +0100

    VERIFICACION

---
+++ VERIFICACION/ROM_tb.sv
@@ -0,0 +1,23 @@
+`timescale 1 ns/ 1 ps
+module ROM_tb();
+reg CLK;
+reg [31:0]address_instr; 
+reg [31:0]instr;
+ROM ROM_ins(	.CLK(CLK),
+		.INS_ADDRESS(address), 
+		.INSTRUCTION(instr)
+);
+//RELOJ
+initial                                                
+begin   
+CLK=1'b0;                                               
+forever # 10 CLK=!CLK;                    
+end 
+
+initial
+begin
+$readmemh("codigo_fibonacci.txt")
+end
+$stop
+
+endmodule
