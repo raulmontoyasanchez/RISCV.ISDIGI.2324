6677b77b19d13236d98675ab1995cb070534b50b
dgargon2 <dgarglez03@gmail.com>
2023-12-09 13:55:55 +0100

    VERIFICACION

--- VERIFICACION/SINGLECYCLE_tb.sv
+++ VERIFICACION/SINGLECYCLE_tb.sv
@@ -3,7 +3,24 @@ module SINGLECYCLE();
 logic CLK,RST;
 
 
-TOP TOP_inst
+TOP TOP_inst( 
+                .ROM_INS(ROM_INS),     
+                .RAM_DATA(RAM_DATA), 
+                .ROM_ADD(ROM_ADD), 
+                .RAM_ADD(RAM_ADD) 
+                .CLK(CLK), 
+                .RST(RST)
+);
 
 
+//Clock generation
+always 
+    begin 
+		#(T/2) CLK = ~CLK;
+	end
+
+initial begin
+$readmemh("W:\\A ISDIGT FINAL\\PRUEB-POLILABS\\codigo_fibonacci.txt",MRAM);
+$readmemh("W:\\A ISDIGT FINAL\\PRUEB-POLILABS\\codigo_fibonacci.txt",MROM);    
+end
 endmodule
