177745210e720c4165eb21bdfa8142a872c8d4f6
ElenaFructuoso <elenafructuosovalenti@gmail.com>
2024-01-03 18:56:27 +0100

    dfassd

--- SINGLE-CYCLE (FASE 1-2)/HARDWARE/ALU.sv
+++ SINGLE-CYCLE (FASE 1-2)/HARDWARE/ALU.sv
@@ -11,13 +11,13 @@ always_comb
 		case(OPERATION)
 
 		4'b0000: RESULT = A+B;			//SUMA
-		4'b0001: RESULT = A&B;			//AND
-		4'b0010: RESULT = A | B;		//OR
-		4'b0011: RESULT = A << B;		//Desplazamiento izquierda (A tantos desplazamientos como indique B)
-		4'b0100: RESULT = (A<B) ? 1:0;//A menor que B = 1
+		4'b0111: RESULT = A&B;			//AND
+		4'b0110: RESULT = A | B;		//OR
+		4'b0001: RESULT = A << B;		//Desplazamiento izquierda (A tantos desplazamientos como indique B)
+		4'b0010: RESULT = (A<B) ? 1:0;//A menor que B = 1
 		4'b0101: RESULT = A >> B;		//Desplazamiento derecha lógico (A tantos desplazamientos como indique B)
-		4'b0110: RESULT = A-B;			//RESTA
-		4'b0111: RESULT = A^B;			//XOR
+		4'b1000: RESULT = A-B;			//RESTA
+		4'b0100: RESULT = A^B;			//XOR
 
 		//INSTRUCCIONES DE SALTO CONDICIONAL (B-FORMAT)
 		//FORZAMOS QUE RESULT SEA IGUAL A CERO PARA ACTIVAR EL FLAG 
